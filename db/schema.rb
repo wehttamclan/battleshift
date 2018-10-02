df6e13738d49bd9216051e5dfd1b32a5bb751b9c
Josh Mejia <joshua.mejia@gmail.com>
2018-10-02 14:17:53 -0600

    Add users show and index to the API

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313201116) do
+ActiveRecord::Schema.define(version: 20181001214617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20180313201116) do
     t.integer "player_1_turns"
     t.integer "player_2_turns"
     t.integer "current_turn"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "address"
+    t.integer "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/requests/users_request_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe "users api endpoints" do
+  context "GET /api/v1/users" do
+    it "returns a list of users" do
+      create_list(:user, 2)
+
+      get "/api/v1/users"
+
+      expect(response.status).to be 200
+
+      users = JSON.parse(response.body, symbolize_names: true)
+      user  = users.first
+
+      expect(users.count).to eq(2)
+      expect(user.keys.count).to eq(3)
+      expect(user).to have_key(:name)
+      expect(user).to have_key(:email)
+      expect(user).to have_key(:id)
+    end
+  end
+
+  context "GET /api/v1/users/:id" do
+    it "returns a specific user" do
+      cj   = User.create!(name: "C.J. Cregg", id: 111)
+      toby = User.create!(name: "Toby Ziegler", id: 222)
+
+      get "/api/v1/users/#{cj.id}"
+
+      expect(response.status).to be 200
+
+      returned_user = JSON.parse(response.body, symbolize_names: true)
+
+      expect(returned_user.keys.count).to eq(3)
+      expect(returned_user).to have_key(:name)
+      expect(returned_user).to have_key(:email)
+      expect(returned_user).to have_key(:id)
+    end
+  end
+end
