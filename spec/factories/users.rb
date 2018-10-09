ce27f6d1959e67e677e53f6566144b5c44c3c5fa
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-10-08 18:22:06 -0600

    Fixes factories, skips a test.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name "Josiah Bartlet"
-    email "jbartlet@example.com"
-    address "1600 Pennsylvania Ave NW, Washington, DC 20500"
-    phone 555555555
+    name {"Josiah Bartlet"}
+    email {"jbartlet@example.com"}
+    address {"1600 Pennsylvania Ave NW, Washington, DC 20500"}
+    phone {555555555}
   end
 end
