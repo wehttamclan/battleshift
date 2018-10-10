715a3cae4b2fa86fa3333205be985519b91200e4
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-10-10 10:38:23 -0600

    Adds test for patching user email via api.

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -19,4 +19,24 @@ describe 'A guest user' do
       expect(page).to have_content('jbarlet@example.com')
     end
   end
+  it 'updates a users email' do
+    visit '/users'
+
+    within(first(".user")) do
+      click_on 'Edit'
+    end
+
+    expect(current_path).to eq '/users/1/edit'
+
+    fill_in 'Email', with: 'josiah@example.com'
+
+    click_on 'Save'
+
+    expect(current_path).to eq '/users'
+
+    expect(page).to have_content "Successfully updated Josiah Bartlet."
+    
+    expect(page).to have_content 'josiah@example.com'
+    expect(page).to_not have_content 'jbarlet@example.com'
+  end
 end
