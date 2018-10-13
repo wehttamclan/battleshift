4142af1491ae3748bf6c33f4a18185d77bf24292
bghalami <benjaminghalami@gmail.com>
2018-10-13 13:44:55 -0600

    created email tests

---
+++ spec/features/registering_triggers_email_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "I Successfully make an account" do
+  it "sends an activation email" do
+    
+  end
+  describe "User clicks activation link" do
+    it "should activate account" do
+
+    end
+  end
+end
+
+# Background: The registration process will trigger this story
+#
+# As a non-activated user
+# When I check my email for the registration email
+# I should see a message that says "Visit here to activate your account."
+# And when I click on that link
+# Then I should be taken to a page that says "Thank you! Your account is now activated."
+#
+# And when I visit "/dashboard"
+# Then I should see "Status: Active"
