54bea1b612abe26fb08269e3b2b36b67f58fa5fb
bghalami <benjaminghalami@gmail.com>
2018-10-13 14:42:09 -0600

    We are beginning to feel stronger

---
+++ app/mailers/activation_notifier_mailer.rb
@@ -0,0 +1,2 @@
+class ActivationNotifierMailer < ApplicationMailer
+end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'noreply@battleship.com'
   layout 'mailer'
 end
