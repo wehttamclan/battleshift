4a8607c5b86fa75ab9604923656e89e87f69e5d3
bghalami <benjaminghalami@gmail.com>
2018-10-13 16:06:04 -0600

    lets try this...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,23 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "battleship_web_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  module YouveChangedV2
+    class Application < Rails::Application
+      config.action_mailer.delivery_method = :smtp
+
+      config.action_mailer.smtp_settings = {
+        address:              'smtp.sendgrid.net',
+        port:                 '587',
+        domain:               'example.com',
+        user_name:            ENV["SENDGRID_USERNAME"],
+        password:             ENV["SENDGRID_PASSWORD"],
+        authentication:       'plain',
+        enable_starttls_auto: true
+      }
+
+
+    end
+  end
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
