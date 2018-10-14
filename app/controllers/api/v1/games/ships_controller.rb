8450b311acec34524f1e41f134f63ae3df0cb44f
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-10-14 17:25:29 -0600

    Ready to add api keys to the game table.

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -1,16 +1,16 @@
 class Api::V1::Games::ShipsController < ApiController
   def create
     game = Game.find(params[:game_id])
-    
-    board_1 = game.player_1_board
-    board_2 = game.player_2_board
-    
+    board = game.player_1_board
     ship = Ship.new(ship_params[:ship_size])
     
     ship.place(ship_params[:start_space], ship_params[:end_space])
-    ShipPlacer.new(board:board_1, ship:ship, start_space:ship.start_space, end_space:ship.end_space).run
+    ShipPlacer.new(board:board, ship:ship, start_space:ship.start_space, end_space:ship.end_space).run
     
-
+    game.save
+    
+    render json: game
+    byebug
   end
 
   private
--- spec/requests/user_can_place_ships_spec.rb
+++ spec/requests/user_can_place_ships_spec.rb
@@ -4,9 +4,13 @@ require 'rails_helper'
 describe 'POST /api/v1/games/:id/ships' do
   context 'to place a ship' do
     it 'and return messages' do
+      user_1 = create(:user, activated: 1)
+      user_2 = create(:user, activated: 1, email: 'opp@email.com')
+      
       game = create(:game)
 
-      headers = { "CONTENT_TYPE" => "application/json" }
+      headers = { "CONTENT_TYPE" => "application/json",
+                  "X-API-Key" => user_1.api_key}
 
       ship_1_payload = {ship_size: 3,
                         start_space: "A1",
@@ -16,8 +20,7 @@ describe 'POST /api/v1/games/:id/ships' do
       
       game = JSON.parse(response.body, symbolize_names: true)
 
-      expect(Game.count).to eq 1
-      expect(response).to be_successful
+      expect(response.status).to eq 204
       expect(game).to have_key(:player_1_board)
       expect(game).to have_key(:current_turn)
     end
