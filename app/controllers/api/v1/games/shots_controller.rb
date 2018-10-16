61625b52ef35a6df7c959950caffca34c28c97e4
bghalami <benjaminghalami@gmail.com>
2018-10-15 22:13:13 -0600

    refactored the shots controller further

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -5,9 +5,7 @@ module Api
         def create
           @game = Game.find(params[:game_id])
 
-          if @game.current_turn == "player_1" && request.headers["X-API-KEY"] == @game.player_1_api_key
-            make_move
-          elsif @game.current_turn == "player_2" && request.headers["X-API-KEY"] == @game.player_2_api_key
+          if player_1_move? || player_2_move?
             make_move
           else
             render json: @game, status: 400, message: "Invalid move. It's your opponent's turn"
@@ -20,6 +18,14 @@ module Api
           turn_processor = TurnProcessor.new(@game, params[:shot][:target])
           turn_processor.run!
           render json: @game, message: turn_processor.message
+        end
+
+        def player_1_move?
+          @game.current_turn == "player_1" && request.headers["X-API-KEY"] == @game.player_1_api_key
+        end
+
+        def player_2_move?
+          @game.current_turn == "player_2" && request.headers["X-API-KEY"] == @game.player_2_api_key
         end
       end
     end
