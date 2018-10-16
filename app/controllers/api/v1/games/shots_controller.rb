6665658f5756e899b923e27bba04855e75ac9478
bghalami <benjaminghalami@gmail.com>
2018-10-15 22:33:31 -0600

    added a tab

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -6,11 +6,11 @@ module Api
           @game = Game.find(params[:game_id])
 
           if player_1_move? || player_2_move?
-            if shot_on_board?
-              make_move
-            else
-              render json: @game, status: 400, message: "Invalid coordinates"
-            end
+              if shot_on_board?
+                make_move
+              else
+                render json: @game, status: 400, message: "Invalid coordinates"
+              end
           else
             render json: @game, status: 400, message: "Invalid move. It's your opponent's turn"
           end
