c502e3aa0e55e4729d8a465bf365124269081254
bghalami <benjaminghalami@gmail.com>
2018-10-15 22:40:38 -0600

    refactored ShipsController, not sure if it's easier to read...

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -1,34 +1,43 @@
 class Api::V1::Games::ShipsController < ApiController
   def create
-    game = Game.find(params[:game_id])
+    @game = Game.find(params[:game_id])
+    @ship = Ship.new(ship_params[:ship_size])
 
-    if request.headers["X-API-Key"] == game.player_1_api_key
-      board = game.player_1_board
-    elsif request.headers["X-API-Key"] == game.player_2_api_key
-      board = game.player_2_board
-    end
+    @ship.place(ship_params[:start_space], ship_params[:end_space])
+    board_picker
+    place_ship
 
-    ship = Ship.new(ship_params[:ship_size])
+    @board.ship_math(ship_params[:ship_size])
 
-    ship.place(ship_params[:start_space], ship_params[:end_space])
-    ShipPlacer.new(board:board,
-                   ship:ship,
-                   start_space:ship.start_space,
-                   end_space:ship.end_space
-                 ).run
+    @game.save
 
-    board.ship_math(ship_params[:ship_size])
-
-    option = " with a size of #{board.ship_places}"
-    message = "Successfully placed ship with a size of #{ship_params[:ship_size]}. You have #{board.ship_count} ship(s) to place#{option if board.ship_count > 0 }."
-
-    game.save
-
-    render json: game, message: message
+    render json: @game, message: ship_message
   end
 
   private
+  
   def ship_params
     params.require(:ship).permit(:ship_size, :start_space, :end_space)
+  end
+
+  def board_picker
+    if request.headers["X-API-Key"] == @game.player_1_api_key
+      @board = @game.player_1_board
+    elsif request.headers["X-API-Key"] == @game.player_2_api_key
+      @board = @game.player_2_board
+    end
+  end
+
+  def place_ship
+    ShipPlacer.new( board:       @board,
+                    ship:        @ship,
+                    start_space: @ship.start_space,
+                    end_space:   @ship.end_space
+                  ).run
+  end
+
+  def ship_message
+    option = " with a size of #{@board.ship_places}"
+    "Successfully placed ship with a size of #{ship_params[:ship_size]}. You have #{@board.ship_count} ship(s) to place#{option if @board.ship_count > 0 }."
   end
 end
