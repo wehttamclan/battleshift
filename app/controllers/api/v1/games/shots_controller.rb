7c093e5b2b7b6b4750023cef0ec2639f6e11e7dc
bghalami <benjaminghalami@gmail.com>
2018-10-15 22:10:47 -0600

    refactored the shots controller

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -3,22 +3,23 @@ module Api
     module Games
       class ShotsController < ApiController
         def create
-          game = Game.find(params[:game_id])
+          @game = Game.find(params[:game_id])
 
-          if game.current_turn == "player_1" && request.headers["X-API-KEY"] == game.player_1_api_key
-            turn_processor = TurnProcessor.new(game, params[:shot][:target])
-            turn_processor.run!
-            message = turn_processor.message
-            render json: game, message: message
-          elsif game.current_turn == "player_2" && request.headers["X-API-KEY"] == game.player_2_api_key
-            turn_processor = TurnProcessor.new(game, params[:shot][:target])
-            turn_processor.run!
-            message = turn_processor.message
-            render json: game, message: message
+          if @game.current_turn == "player_1" && request.headers["X-API-KEY"] == @game.player_1_api_key
+            make_move
+          elsif @game.current_turn == "player_2" && request.headers["X-API-KEY"] == @game.player_2_api_key
+            make_move
           else
-            message = "Invalid move. It's your opponent's turn"
-            render json: game, status: 400, message: message
+            render json: @game, status: 400, message: "Invalid move. It's your opponent's turn"
           end
+        end
+
+        private
+
+        def make_move
+          turn_processor = TurnProcessor.new(@game, params[:shot][:target])
+          turn_processor.run!
+          render json: @game, message: turn_processor.message
         end
       end
     end
