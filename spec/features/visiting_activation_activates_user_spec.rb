26a638f330aa1d92f6904da9ed93d664f515f4df
bghalami <benjaminghalami@gmail.com>
2018-10-18 15:52:59 -0600

    PUSHING TO MASTER

--- spec/features/visiting_activation_activates_user_spec.rb
+++ spec/features/visiting_activation_activates_user_spec.rb
@@ -16,15 +16,5 @@ RSpec.describe ActivateController, type: :controller do
 
       expect(current_path).to eq("/dashboard")
     end
-
-    it "redirects to dashboard" do
-      VCR.use_cassette("activating account and redirecting to dashboard111110") do
-        visit "/dashboard"
-      end
-
-      expect(current_path).to eq("/dashboard")
-      binding.pry
-      expect(@user.activated).to eq("active")
-    end
   end
 end
