489585cc3ccc22143ab2cabee09f8a0895d6fd4b
bghalami <benjaminghalami@gmail.com>
2018-10-18 15:34:29 -0600

    all this spec harness, so little time

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -2,16 +2,11 @@ module Api
   module V1
     module Games
       class ShotsController < ApiController
-        # before_action :validate_user
+        before_action :validate_user
         def create
-          # IT CAN'T WORK WITH INACTIVE PLAYERS
-          # NEEDS 401 error IF API IS INVALID
-          # NEEDS 401 error IF GAME IS NOT YOURS
           target  = params[:shot][:target]
           api_key = request.headers["X-API-KEY"]
-
           turn_processor = TurnProcessor.new(game, target, api_key)
-
           turn_processor.process_turn
 
           render json: game, status: turn_processor.status, message: turn_processor.message
@@ -22,18 +17,17 @@ module Api
         def game
           Game.find(params[:game_id])
         end
-        #
-        # def validate_user
-        #   unless valid_user?
-        #     render status: 401 , message: "Unauthorized"
-        #   end
-        # end
-        #
-        # def valid_user?
-        #   game = Game.find(params[:game_id])
-        #   incoming_api = request.headers["X-API-KEY"]
-        #   game.player_1_api_key == incoming_api || game.player_2_api_key == incoming_api
-        # end
+
+        def validate_user
+          unless valid_user?
+            render json: game, status: 401 , message: "Unauthorized"
+          end
+        end
+
+        def valid_user?
+          incoming_api = request.headers["X-API-KEY"]
+          game.player_1_api_key == incoming_api || game.player_2_api_key == incoming_api
+        end
       end
     end
   end
